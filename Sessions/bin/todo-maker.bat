1a1c5ab50a425cbed8883f29e9c32f7e26a6bd33
Paul Carvalho <tester.paul@gmail.com>
2011-06-11 16:48:11 -0400

    what's in a name?

--- Sessions/bin/todo-maker.bat
+++ Sessions/bin/todo-maker.bat
@@ -5,4 +5,4 @@
 :: Run this batch file from C:\Sessions
 :: Input file is todo.xls in the C:\Sessions\Todo folder, *not* the TXT file.
 
-bin\todomaker_win.rb config todo\todo.xls
+bin\todomaker_win_xls.rb config todo\todo.xls
