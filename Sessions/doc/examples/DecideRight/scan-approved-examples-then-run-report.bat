6a5a3646e706d8c44c81d7c23b77f358ee58147c
Paul Carvalho <tester.paul@gmail.com>
2011-06-13 20:06:18 -0400

    what's up doc?

---
+++ Sessions/doc/examples/DecideRight/scan-approved-examples-then-run-report.bat
@@ -0,0 +1,27 @@
+@ECHO OFF
+::
+:: scan-approved-then-run-report.bat
+::
+
+: Remove previous report files before generating new ones (Optional, but nice to do)
+: (NOTE: This _assumes_ the files are in the 'reports' subfolder)
+
+DEL reports\breakdowns*.txt
+DEL reports\charters*.txt
+DEL reports\data.txt
+DEL reports\bugs.txt
+DEL reports\issues.txt
+DEL reports\test*.txt
+DEL reports\*.htm
+
+
+: Check the file integrity of the approved sheets. Create summary files.
+
+..\..\..\bin\scan2.rb config approved
+START scan.log
+
+
+: Generate HTML reports
+
+..\..\..\bin\report2.rb config
+START reports\status.htm
