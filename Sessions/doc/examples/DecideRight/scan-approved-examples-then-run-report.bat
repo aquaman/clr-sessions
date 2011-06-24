bfd2cfb5e2fb63ca827a1bffbcbdd058e2bad5f1
Paul Carvalho <tester.paul@gmail.com>
2011-06-23 23:35:24 -0400

    examplification amplification

--- Sessions/doc/examples/DecideRight/scan-approved-examples-then-run-report.bat
+++ Sessions/doc/examples/DecideRight/scan-approved-examples-then-run-report.bat
@@ -3,17 +3,12 @@
 :: scan-approved-then-run-report.bat
 ::
 
-: Remove previous report files before generating new ones (Optional, but nice to do)
-: (NOTE: This _assumes_ the files are in the 'reports' subfolder)
-
-DEL reports\breakdowns*.txt
-DEL reports\charters*.txt
-DEL reports\data.txt
-DEL reports\bugs.txt
-DEL reports\issues.txt
-DEL reports\test*.txt
-DEL reports\*.htm
-
+: 
+: This is a sample Windows script that you can use to automate several
+: actions at once.  For example, this script does the following:
+: * scan files and display results
+: * generate HTML report and display it in web browser
+: 
 
 : Check the file integrity of the approved sheets. Create summary files.
 
