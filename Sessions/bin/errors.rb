8fe947af4441fdaa0d198abafffb0b8bf21d0f0a
Paul Carvalho <tester.paul@gmail.com>
2020-02-24 19:03:42 -0500

    Update errors.rb to include multi-line error messages

--- Sessions/bin/errors.rb
+++ Sessions/bin/errors.rb
@@ -5,17 +5,30 @@
 # Purpose: to produce a list of error messages used in the given input file (e.g. SCAN.RB)
 # ----------------------------------------------------------------------------------------------------------- #
 # Ported from the original PERL script (dated 29-May-2000) to RUBY by Paul Carvalho
-# Last Updated: 01 May 2007
+# Last Updated: 24 February 2020
 # =========================================================================================================== #
 
 if ( ARGV[0].nil? or ( ! File.exist?( ARGV[0] ) ) )
-	puts "Specify the location of the [scan.*] file to scan for 'error' messages."
-	exit
+  puts "Specify the location of the [scan.*] file to scan for 'error' messages."
+  exit
 end
 
 error_list = []
 while gets
-	error_list << $1 if $_ =~ /.+?error\(\s*\"(.+?)\"\s*\)/
+  error_msg_found = false
+  error_msg = ''
+  if $_ =~ /.+?error\(\s*[\"|\'](.+?)[\"|\']\s*\)/
+    # Single-line error message
+    error_msg = $1
+    error_msg_found = true
+  elsif $_ =~ /.+?error\(\s*[\"|\'](.+?)[\"|\']\s*\+/
+    # Multi-line error message
+    error_msg = $1
+    gets =~ /\s*[\"|\'](.+?)[\"|\']\s*\)/
+    error_msg << $1
+    error_msg_found = true
+  end
+  error_list << error_msg if error_msg_found
 end
 
 f_ERRORS = File.new("errors.txt", "w")
