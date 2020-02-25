4393af0a6852bd14fb808de1093e45c69fd1a886
Paul Carvalho <tester.paul@gmail.com>
2020-02-24 19:17:02 -0500

    de-tabification

--- Sessions/bin/recent.rb
+++ Sessions/bin/recent.rb
@@ -18,24 +18,24 @@
 ### METHODS ###
 
 def format_num( tmp_arr )
-	tmp_arr.collect! do |x|
-		if ( ( x.to_f.to_s == x ) and ( x.to_f == x.to_i ) )     # (integer value - no decimals)
-			x.to_i.to_s
-		elsif ( x.to_f.to_s == x )     # (decimal value - format to 2 decimals)
-			sprintf("%0.2f", x)
-		else
-			x
-		end
-	end
+  tmp_arr.collect! do |x|
+    if ( ( x.to_f.to_s == x ) and ( x.to_f == x.to_i ) )     # (integer value - no decimals)
+      x.to_i.to_s
+    elsif ( x.to_f.to_s == x )     # (decimal value - format to 2 decimals)
+      sprintf("%0.2f", x)
+    else
+      x
+    end
+  end
 end
 
 ### START ###
 
 begin
-	f_DAYBREAKS = File.open( "../reports/breakdowns-day.txt" )
+  f_DAYBREAKS = File.open( "../reports/breakdowns-day.txt" )
 rescue
-	puts "Can't find 'breakdowns-day.txt' file."
-	exit
+  puts "Can't find 'breakdowns-day.txt' file."
+  exit
 end
 f_DAYBREAKS.gets     # (Discard the heading row)
 
@@ -43,20 +43,20 @@ puts "\nThese are the last #{number} days of test session progress"
 puts Time.now.strftime("as of %m/%d/%y at %I:%M%p:").downcase
 
 number.times do
-	break if f_DAYBREAKS.eof?
-	#($date,$total,$oncharter,$opportunity,$test,$bug,$prep,$pertester,$bugs,$issues)  << this is the original line, but there is *NO* 'per tester' field/value in this data file!
-	( date, total, oncharter, opportunity, test, bug, prep, bugs, issues ) = format_num( f_DAYBREAKS.gets.split('"').delete_if {|x| x.strip.empty? } )
-	puts
-	puts "        Date: #{date}"
-	puts "       Total: #{total}"
-	puts "  On Charter: #{oncharter}"
-	puts " Opportunity: #{opportunity}"
-	puts "        Test: #{test}"
-	puts "         Bug: #{bug}"
-	puts "       Setup: #{prep}"
-#	puts "  Per Tester: #{pertester}"
-	puts "        Bugs: #{bugs}"
-	puts "      Issues: #{issues}"
+  break if f_DAYBREAKS.eof?
+  #($date,$total,$oncharter,$opportunity,$test,$bug,$prep,$pertester,$bugs,$issues)  << this is the original line, but there is *NO* 'per tester' field/value in this data file!
+  ( date, total, oncharter, opportunity, test, bug, prep, bugs, issues ) = format_num( f_DAYBREAKS.gets.split('"').delete_if {|x| x.strip.empty? } )
+  puts
+  puts "        Date: #{date}"
+  puts "       Total: #{total}"
+  puts "  On Charter: #{oncharter}"
+  puts " Opportunity: #{opportunity}"
+  puts "        Test: #{test}"
+  puts "         Bug: #{bug}"
+  puts "       Setup: #{prep}"
+#  puts "  Per Tester: #{pertester}"
+  puts "        Bugs: #{bugs}"
+  puts "      Issues: #{issues}"
 end
 
 puts
